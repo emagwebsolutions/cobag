04bda480bfdc538cb664d672e4f8ce07c6703306
Emmanuel Agyemang <emagweb2017@gmail.com>
2022-05-16 10:14:26 +0000

    Fifth Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,330 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className="home">
 
-      THIS IS THE HOME PAGE
+      <section>
+        <div>
+          <div className="logo">
+          <img src="/logo.jpg" alt="" />
+          </div>
+
+          <h1>COUNCIL OF BISHOPS</h1>
+          <h1>AND APOSTLES, GHANA</h1>
+          <h3>The Fear of God is the begining of wisdom</h3>
+          <a href=""> APPLY NOW </a>
+        </div>
+      </section>
+
+      <section>
+        Training the next generation of Bishops & Apostles
+      </section>
+
+
+      <section  className="about-section">
+        <div className="container">
+          <div>
+            <h3>BISHOP SOLOMON AKWASI SARPONG</h3>
+          <div className="bishop">
+
+          </div>
+          </div>
+
+          <div>
+          <h3>ABOUT COBAG</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam amet corporis tempora illo ducimus ad ut. Ducimus, commodi veritatis, voluptatum accusamus unde exercitationem iste voluptatem ex odio illum quos quibusdam.
+            </p>
+            <a href="/">More about our bible school</a>
+          </div>
+        </div>
+      </section>
+
+
+      <section className="cobag-life">
+
+        <div className="container">
+          <div>
+        <h1>WHAT'S IT LIKE AT COBAG</h1>
+        <div className="divider"></div>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, laudantium ab explicabo neque nisi </p>
+
+        <div className="imgs flex">
+
+        <div>
+            <div  className="enquiry">
+            <div>
+              <article>
+              <h5>What a </h5>
+              <h2>Student</h2>
+              <h6>want to know</h6>
+              </article>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="enquiry">
+            <div>
+              <article>
+              <h5>What a </h5>
+              <h2>Student</h2>
+              <h6>want to know</h6>
+              </article>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div  className="enquiry">
+            <div>
+              <article>
+              <h5>What a </h5>
+              <h2>Student</h2>
+              <h6>want to know</h6>
+              </article>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        </div>
+        </div>
+
+
+      </section>
+
+
+      <section className="large-img"></section>
+
+
+      <section className="student-testimonials">
+
+        <div className="container">
+          <div>
+            <h1 className="text-center">STUDENT TESTIMONIALS</h1>
+            <div className="divider"></div>
+            <div className="flex text-center">
+
+              <div className="p-3">
+                <picture>
+                  <img src="" alt="" />
+                </picture>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos quia facilis earum mollitia quidem illo ipsam quam laudantium culpa sequi optio quo iusto autem perferendis odio, illum, officia temporibus quaerat.
+                </p>
+              </div>
+
+              <div className="p-3">
+                <picture>
+                  <img src="" alt="" />
+                </picture>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos quia facilis earum mollitia quidem illo ipsam quam laudantium culpa sequi optio quo iusto autem perferendis odio, illum, officia temporibus quaerat.
+                </p>
+              </div>
+
+              <div className="p-3">
+                <picture>
+                  <img src="" alt="" />
+                </picture>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos quia facilis earum mollitia quidem illo ipsam quam laudantium culpa sequi optio quo iusto autem perferendis odio, illum, officia temporibus quaerat.
+                </p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+
+      <section className="staff">
+
+        <div className="container">
+          <div>
+            <h1>OUR STAFF</h1>
+            <div className="divider"></div>
+
+            <div className="flex">
+
+              <div>
+                <picture>
+                  <img src="" alt="" />
+                </picture>
+                <h2>Full Name</h2>
+                <p>Role</p>
+              </div>
+
+              <div>
+                <picture>
+                  <img src="" alt="" />
+                </picture>
+                <h2>Full Name</h2>
+                <p>Role</p>
+              </div>
+
+              <div>
+                <picture>
+                  <img src="" alt="" />
+                </picture>
+                <h2>Full Name</h2>
+                <p>Role</p>
+              </div>
+
+
+
+
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+
+      <section className="contactSection">
+        <div className="container">
+          <div>
+            <h1>CONTACT</h1>
+            <div className="divider"></div>
+            <p>
+            Get in touch with us by using the form below, or using the details below.
+            Alternatively – email us at: office@capernwrayaustralia.org
+            Sending post to a student: Please add the student’s name as heading, when addressing.
+
+            Office Hours Monday – Friday 9am- 4:00pm (Closed Wednesday)
+            </p>
+            <div className="flex">
+
+              <div className="flex">
+
+
+                <div>
+                  <div className="flex">
+                    <div>
+                      <h2>Postal Address</h2>
+                        <p>
+                        Capernwray Torchbearers Australia
+                        PO Box 210,
+                        Moss Vale, NSW
+                        Australia 2577
+                        </p>
+                    </div>
+                    <div>
+                      <h2>Street Address</h2>
+                      <p>
+                        Capernwray Torchbearers Australia
+                        PO Box 210,
+                        Moss Vale, NSW
+                        Australia 2577
+                        </p>
+                    </div>
+                  </div>
+
+                  <div className="flex">
+                    <div>
+                      <h2>Postal Address</h2>
+                        <p>
+                        Capernwray Torchbearers Australia
+                        PO Box 210,
+                        Moss Vale, NSW
+                        Australia 2577
+                        </p>
+                    </div>
+                    <div>
+                      <h2>Street Address</h2>
+                      <p>
+                        Capernwray Torchbearers Australia
+                        PO Box 210,
+                        Moss Vale, NSW
+                        Australia 2577
+                        </p>
+                    </div>
+                  </div>
+
+                </div>
+                 <div>
+                    <h1>Contact Us</h1>
+                   <form action="">
+<input type="text" placeholder='First Name' />
+<input type="text" placeholder='Last Name' />
+<input type="text" placeholder='Email' />
+<textarea name="" id="" cols="30" rows="10"></textarea>
+<a href="">SEND</a>
+
+                   </form>
+                  
+                </div>
+
+
+              </div>
+              <div>
+                
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+
+      <section></section>
+
+      <style jsx>
+        {`
+          .home section:nth-of-type(1){
+            width: 100vw;
+            height: 100vh;
+            background-image: url('/cover.jpg');
+            background-size: cover;
+            background-position: center;
+            color: #fff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .bishop{
+            height: 40rem;
+            background-image: url('/bishop.jpg');
+            background-size: cover;
+            background-position: center;
+            color: #fff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .enquiry{
+            height: 40rem;
+            background-image: url('/student.jpeg');
+            background-size: cover;
+            background-position: center;
+            color: #fff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .large-img{
+            height: 50rem;
+            background-image: url('/study.jpg');
+            background-size: cover;
+            background-position: center;
+            color: #fff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+        `}
+      </style>
      
-    </>
+    </div>
+
   )
 }
 
